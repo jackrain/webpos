078df0076d75bf954ae3665ef7880ef9e7552e93
Beven91 <zjayzbwen@163.com>
2014-12-17 13:18:39 +0800

    规范化代码，同时完善js时间处理辅助函数

--- program.js
+++ program.js
@@ -3,10 +3,10 @@
 function Run() {
     root.baseDIR = __dirname;
     root.Application = new context.RunApplication();
-    root.Application.Run("test", 'test.module.validate');
+    root.Application.Run("test", 'test.date');
 }
 module.exports.Run = Run;
 
 Run();
 
-setTimeout(function () { debugger;},60000);
+setTimeout(function () { debugger; }, 60000);
